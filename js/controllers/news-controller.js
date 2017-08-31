70535c75bab3bae7d81ca68829b4c568ad93300f
shannonpeng <shannon@dhcp-18-189-47-217.dyn.MIT.EDU>
2017-08-31 04:17:43 -0400

    Basic news page

--- js/controllers/news-controller.js
+++ js/controllers/news-controller.js
@@ -3,5 +3,9 @@ app.controller('news-controller', function($scope, $firebaseArray) {
 
 	$scope.id = "news";
 	$scope.title = "news";
-   
+
+	// import blog posts as Firebase array 
+	var posts = firebase.database().ref("blog");
+	$scope.posts = $firebaseArray(posts);
+	
 });
