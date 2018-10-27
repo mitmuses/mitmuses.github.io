997e0b2c8b08342f4e974be0800eac7083a8f3f7
Shannon Peng <pengs@Shannons-MacBook-Pro.local>
2018-10-27 13:07:30 -0400

    Fix reunion confetti again

--- reunion/scripts/shapes.js
+++ reunion/scripts/shapes.js
@@ -8,12 +8,12 @@ $(document).ready(function() {
   const COLORS = ["#720b54", "#a33e99", "#d2bbea", "#ffd6fa", "#bbe3ed", "#dcdcdc"];
   const CLASSES = ["circle", "triangle", "rectangle"]
 
-  const MAX_TOP = 100;
+  const MAX_TOP = 200;
 
   for (var i = 0; i < NUM_SHAPES; i++) {
     var scale = Math.random()*(MAX_SCALE-MIN_SCALE) + MIN_SCALE;
-    var top = Math.floor(Math.random()*100*MAX_TOP)/100;
-    var left = Math.floor(Math.random()*100*MAX_TOP)/100;
+    var top = Math.floor(Math.random()*100*MAX_TOP)/100-100;
+    var left = Math.floor(Math.random()*100*100)/100;
     var spin = Math.floor(Math.random()*360);
     var color = COLORS[Math.floor(Math.random()*COLORS.length)];
     var type = CLASSES[Math.floor(Math.random()*CLASSES.length)];
