ef8736eb2a3761e2d8bbce8ba4ea040662fdebe2
ruthshiferaw <85802601+ruthshiferaw@users.noreply.github.com>
2022-09-18 19:05:19 -0400

    move 2022 members to alumni

--- js/controllers/members-controller.js
+++ js/controllers/members-controller.js
@@ -112,7 +112,7 @@ app.controller('members-controller', function($scope, $firebaseArray) {
 
 	// setTimeout(function() { $scope.sortMembers() }, 3000);
 	// setTimeout(function() { $scope.sortAlumni() }, 3000);
-	// setTimeout(function() { $scope.migrateAlumniByYear(2022) }, 3000);
+	setTimeout(function() { $scope.migrateAlumniByYear(2022) }, 3000);
 	// setTimeout(function() { $scope.migrateAlumniByNames(['Beyonce', 'Christina Aguilera']) }, 3000);
 	// setTimeout(function() { $scope.migrateMembersByNames(['Beyonce', 'Christina Aguilera']) }, 3000);
 
